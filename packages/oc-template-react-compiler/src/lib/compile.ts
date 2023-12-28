741ca56915018339369e15b1eb9729a2d36eeb95
Ricardo Devis Agullo <ricardo.devisagullo@guestline.com>
2023-12-28 18:41:51 +0100

    use new compiler on react

--- packages/oc-template-react-compiler/src/lib/compile.ts
+++ packages/oc-template-react-compiler/src/lib/compile.ts
@@ -1,25 +1,10 @@
-import { createCompile } from 'oc-generic-template-compiler';
-import compileStatics from 'oc-statics-compiler';
 import { getInfo } from 'oc-template-react';
-import { viteView, viteServer } from 'oc-vite-compiler';
 import react from '@vitejs/plugin-react';
-
+import { createCompile } from 'oc-vite-compiler';
 import reactOCProviderTemplate from './reactOCProviderTemplate';
 
-export type CompilerOptions = ReturnType<typeof createCompile>;
-
-export const compile: CompilerOptions = createCompile({
-  compileView: (options, cb) =>
-    viteView(
-      {
-        ...options,
-        plugins: [(react as any)()],
-        viewWrapper: reactOCProviderTemplate,
-        externals: getInfo().externals
-      },
-      cb
-    ),
-  compileServer: viteServer,
-  compileStatics,
+export const compile = createCompile({
+  plugins: [react() as any],
+  viewWrapper: reactOCProviderTemplate,
   getInfo
 });
