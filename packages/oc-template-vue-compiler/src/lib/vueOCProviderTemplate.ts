837ffa8d90c46909e46493c73c791a546e871de0
Ricardo Devis Agullo <ricardo.devisagullo@guestline.com>
2023-12-29 08:44:12 +0100

    remove removeextension

--- packages/oc-template-vue-compiler/src/lib/vueOCProviderTemplate.ts
+++ packages/oc-template-vue-compiler/src/lib/vueOCProviderTemplate.ts
@@ -1,9 +1,7 @@
-const removeExtension = (path: string) => path.replace(/\.(t|j)sx?$/, '');
-
 export default function vueOCProviderTemplate({ viewPath }: { viewPath: string }) {
   return `
   import { createApp } from 'vue'
-  import View from '${removeExtension(viewPath)}';
+  import View from '${viewPath}';
 
   function renderer(props, element, ssr) {
     createApp(View, props).mount(element, ssr);
