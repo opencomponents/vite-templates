e9f9d7206b46a4632c2e37832c4e85bc91d55709
Ricardo Devis Agullo <ricardo.devisagullo@guestline.com>
2023-12-29 08:50:36 +0100

    scaffold app

--- packages/oc-template-vue-compiler/scaffold/src/src/App.vue
+++ packages/oc-template-vue-compiler/scaffold/src/src/App.vue
@@ -6,12 +6,10 @@ defineProps<{ firstName: string, lastName: string }>()
 
 <template>
   <div>
-    {{ firstName }} 
+    Hello {{ firstName }} 
     <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
     </a>
     <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
   </div>
   <HelloWorld msg="Vite + Vue" />
