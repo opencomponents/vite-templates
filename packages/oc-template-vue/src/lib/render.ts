1e286a2a77af9c8ae4b9dc1ee69811b972ff7d48
Ricardo Devis Agullo <ricardo.devisagullo@guestline.com>
2023-12-31 14:47:55 +0100

    put vue as external

--- packages/oc-template-vue/src/lib/render.ts
+++ packages/oc-template-vue/src/lib/render.ts
@@ -1,47 +1,20 @@
+import Vue from 'vue';
 import { renderToString } from 'vue/server-renderer';
-import createPredicate from './to-be-published/get-js-from-url';
-import tryGetCached from './to-be-published/try-get-cached';
+import { SsrOptions, ssr } from './to-be-published/ssr';
+import { createSSRApp } from 'vue';
 
-export function render(options: any, callback: any) {
-  try {
-    const url = options.model.vueComponent.src;
-    const key = options.model.vueComponent.key;
-    const componentKey = options.model.component.key;
-    const props = options.model.vueComponent.props;
-    const extractor = (key: any, context: any) => context.oc.vueComponents[key];
-    const getJsFromUrl = createPredicate({
-      model: options.model,
-      key,
-      componentKey,
-      url,
-      extractor,
-    });
+import { callbackify } from 'util';
 
-    tryGetCached(
-      'vueComponent',
-      key,
-      getJsFromUrl,
-      (err: any, CachedApp: any) => {
-        if (err) return callback(err);
-        try {
-          renderToString(CachedApp(props))
-            .then((vueHtml) => {
-              const html = options.template(
-                Object.assign({}, options.model, {
-                  __html: vueHtml,
-                })
-              );
-              return callback(null, html);
-            })
-            .catch((error) => {
-              return callback(error);
-            });
-        } catch (error) {
-          return callback(error);
-        }
-      }
-    );
-  } catch (err) {
-    return callback(err);
-  }
-}
+export const render = callbackify((options: SsrOptions) => {
+  const renderer = (App: any, initialData: any) =>
+    renderToString(createSSRApp(App, initialData));
+
+  return ssr({
+    componentName: 'react',
+    options,
+    renderer,
+    globals: {
+      Vue,
+    },
+  });
+});
