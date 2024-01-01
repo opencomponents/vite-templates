706fe1f2e8303efb16bcc7dc4adb1b5a56ba31ff
Ricardo Devis Agullo <ricardo.devisagullo@guestline.com>
2024-01-01 22:51:22 +0100

    ignore warnings compiler

--- packages/oc-vite-compiler/src/index.ts
+++ packages/oc-vite-compiler/src/index.ts
@@ -1,3 +1,5 @@
+process.env.VITE_CJS_IGNORE_WARNING = 'true';
+
 export { default as viteView } from './lib/viteView';
 export { default as viteServer } from './lib/viteServer';
 export { default as createCompile } from './lib/compile';
