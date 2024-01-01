0f3526066c8a13ffb028bd3f130e8a97f936e36a
Ricardo Devis Agullo <ricardo.devisagullo@guestline.com>
2024-01-01 13:55:44 +0100

    Es6 template mod

---
+++ packages/create-oc/templates/es6/src/view.ts
@@ -0,0 +1,33 @@
+import { InitialData, serverClient } from 'oc-server';
+import styles from './styles.css';
+import logo from '../public/logo.png';
+
+declare const window: Window & {
+  fetchMoreData: () => Promise<void>;
+};
+
+export default ({ firstName, lastName, hobbies, born }: InitialData) => {
+  window.fetchMoreData = async () => {
+    const { funFact } = await serverClient.funFact({ year: born });
+    document.querySelector('#funfact')!.innerHTML = funFact;
+  };
+
+  return /* html */ `
+    <div class="${styles.container}">
+      <img width="50" height="50" src="${logo}" alt="Logo" />
+      <h1>
+        Hello, <span>${firstName}</span> ${lastName}
+      </h1>
+      <div class=${styles.info}>
+        <div class=${styles.block}>Born: ${born}</div>
+        <div class=${styles.block}>
+          <div>Hobbies: ${hobbies.map((x) => x.toLowerCase()).join(', ')}</div>
+        </div>
+      </div>
+      <div id="funfact"></div>
+      <button class=${styles.button} onclick="fetchMoreData()">
+        Fun year fact
+      </button>
+    </div>
+  `;
+};
