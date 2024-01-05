8707903174518bf01a9066e22bcb5b649dd163e3
Ricardo Devis Agullo <ricardo.devisagullo@guestline.com>
2024-01-05 14:39:33 +0100

    update compiler

--- packages/oc-server/src/serverClient.ts
+++ packages/oc-server/src/serverClient.ts
@@ -1,3 +1,5 @@
+/// <reference types="oc-vite/client" />
+
 import { AnyServer, RegisteredServer, Action } from './Server';
 
 type InferInput<R> = R extends Action<infer I, any, any> ? I : any;
@@ -7,46 +9,6 @@ type ServerClient<TServer extends AnyServer> = {
   readonly [Property in keyof TServer['actions']]: (
     input: InferInput<TServer['actions'][Property]>
   ) => Promise<InferOutput<TServer['actions'][Property]>>;
-};
-
-function getAction<T>(options: {
-  action: string;
-  componentName: string;
-  componentVersion: string;
-  baseUrl: string;
-  parameters: Record<string, any>;
-}): Promise<T> {
-  return new Promise((resolve, reject) => {
-    (window as any).oc.getData(
-      {
-        action: options.action,
-        name: options.componentName,
-        version: options.componentVersion,
-        baseUrl: options.baseUrl,
-        parameters: options.parameters,
-      },
-      (err: Error | null, data: { component: { props: any } }) => {
-        if (err) {
-          return reject(err);
-        }
-        const {
-          _staticPath,
-          _baseUrl,
-          _componentName,
-          _componentVersion,
-          ...rest
-        } = data.component.props;
-
-        resolve(rest);
-      }
-    );
-  });
-}
-
-declare const window: Window & {
-  oc?: {
-    renderedComponents: Record<string, { baseUrl: string; version: string }>;
-  };
 };
 
 export const serverClient: ServerClient<RegisteredServer> = new Proxy(
@@ -59,14 +21,12 @@ export const serverClient: ServerClient<RegisteredServer> = new Proxy(
         const baseUrl =
           window.oc?.renderedComponents?.[componentName]?.baseUrl ?? '/';
 
-        return getAction({
+        return window.oc.getAction({
           action: prop,
           baseUrl,
-          componentName,
-          componentVersion,
-          parameters: {
-            data,
-          },
+          component: componentName,
+          version: componentVersion,
+          parameters: data,
         });
       };
     },
