f64af50ef556821370d8f5d3fcf88ab55e1d6d56
Ricardo Devis Agullo <mkredz@gmail.com>
2024-04-19 21:17:29 +0200

    fix data on actions

---
+++ .changeset/curvy-pumas-arrive.md
@@ -0,0 +1,11 @@
+---
+'oc-vite-compiler': patch
+'oc-template-elm-compiler': patch
+'oc-template-es6-compiler': patch
+'oc-template-preact-compiler': patch
+'oc-template-react-compiler': patch
+'oc-template-solid-compiler': patch
+'oc-template-vue-compiler': patch
+---
+
+Fix sending arrays or non-objects on actions
--- packages/oc-vite-compiler/src/lib/serverWrapper.ts
+++ packages/oc-vite-compiler/src/lib/serverWrapper.ts
@@ -29,12 +29,16 @@ export const data = (context : any, callback : (error: any, data?: any) => void)
       return callback(error);
     }
     if (model == null) return callback(null, { __oc_emptyResponse: true });
-    const props = Object.assign({}, model, {
-      _staticPath: context.staticPath,
-      _baseUrl: context.baseUrl,
-      _componentName: "${componentName}",
-      _componentVersion: "${componentVersion}"
-    });
+
+    const props = context.action
+      ? model
+      : Object.assign({}, model, {
+          _staticPath: context.staticPath,
+          _baseUrl: context.baseUrl,
+          _componentName: "${componentName}",
+          _componentVersion: "${componentVersion}"
+        });
+
     const srcPathHasProtocol = context.staticPath.indexOf("http") === 0;
     const srcPath = srcPathHasProtocol ? context.staticPath : ("https:" + context.staticPath);
     return callback(null, Object.assign({}, {
