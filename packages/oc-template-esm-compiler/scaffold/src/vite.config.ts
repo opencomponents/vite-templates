b54422de8b7a5ab86cb7adfc8305e9bd6be0d8fe
Ricardo Devis Agullo <mkredz@gmail.com>
2025-07-04 15:09:04 +0200

    update scaffold

--- packages/oc-template-esm-compiler/scaffold/src/vite.config.ts
+++ packages/oc-template-esm-compiler/scaffold/src/vite.config.ts
@@ -1,9 +1,22 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   // Add your own plugins here for thinkgs like eslint or typecheckers
-  plugins: [],
+  plugins: [react()],
+  resolve: {
+    alias: {
+      react: "https://esm.sh/react@19.1.0",
+      "react-dom": "https://esm.sh/react-dom@19.1.0",
+      "react-dom/": "https://esm.sh/react-dom@19.1.0/",
+    },
+  },
+  build: {
+    rollupOptions: {
+      external: ["react", "react-dom"],
+    },
+  },
   // @ts-ignore Missing test property
   test: {
     environment: 'jsdom'
