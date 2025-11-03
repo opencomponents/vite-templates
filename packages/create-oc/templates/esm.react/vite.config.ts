f285d0b0848945be5b5c2a15d160a3c798945a5a
Ricardo Devis Agullo <mkredz@gmail.com>
2025-11-03 14:08:53 +0100

    use vite plugin one sm

--- packages/create-oc/templates/esm.react/vite.config.ts
+++ packages/create-oc/templates/esm.react/vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { oc } from 'oc-server/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   // Add your own plugins here for thinkgs like eslint or typecheckers
-  plugins: [react()],
+  plugins: [oc(), react()],
   build: {
     rollupOptions: {
       external: ['react', 'react-dom'],
--- packages/create-oc/templates/esm.remix/vite.config.ts
+++ packages/create-oc/templates/esm.remix/vite.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite';
+import { oc } from 'oc-server/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  plugins: [oc()],
   build: {
     rollupOptions: {
       external: ['@remix-run/dom', '@remix-run/events'],
